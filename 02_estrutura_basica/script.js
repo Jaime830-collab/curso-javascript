8a86f1f15bd56de004c79ce97b5ff31ea98b244f
Jaime Vaz Nogueira <81386572+Jaime830-collab@users.noreply.github.com>
2024-04-27 11:16:40 -0300

    atualizando

---
+++ 02_estrutura_basica/script.js
@@ -0,0 +1,11 @@
+// falsy
+
+console.log(0 ? "verdadeira" : "falso");
+console.log("" ? "verdadeira" : "falso");
+console.log(null ? "verdadeira" : "falso");
+
+// truthy
+
+console.log(1 ? "verdadeira" : "falso");
+console.log("0" ? "verdadeira" : "falso");
+console.log("abc" ? "verdadeira" : "falso");
